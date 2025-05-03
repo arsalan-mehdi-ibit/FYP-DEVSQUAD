38fb3c26e150fc9a2502258ca1a137f16d5a62a4
Arsalan Mehdi <haishamfaizan@gmail.com>
2025-05-03 20:48:38 +0500

    Scheduling remiders changing

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-Schedule::command('app:emailremindersforsubmittedtimesheets')->monthlyOn(1, '9:00');
-Schedule::command('app:emailremindersforpendingtimesheets')->monthlyOn(1, '9:00');
-Schedule::command('app:emailremindersforrejectedtimesheets')->monthlyOn(1, '9:00');
+$schedule->command('app:emailremindersforsubmittedtimesheets')->weeklyOn(1, '9:00');
+$schedule->command('app:emailremindersforpendingtimesheets')->weeklyOn(1, '9:00');
+$schedule->command('app:emailremindersforrejectedtimesheets')->weeklyOn(1, '9:00');
